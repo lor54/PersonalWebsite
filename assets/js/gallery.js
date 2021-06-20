5c015e62d59e12c975260760e2ede5ba198037fd
lor54 <lorenzocontessa.dev@gmail.com>
2021-06-20 22:31:47 +0200

    Other projects added

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,3 +1,32 @@
+function stopVideos() {
+	$('video').off('play').on('play', function() {
+		var dd = this.id
+		$('video').each(function( index ) {
+			if(dd != this.id){
+				this.pause();
+				this.currentTime = 0;
+			}
+		});
+	});
+}
+
 document.addEventListener( 'DOMContentLoaded', function () {
-	new Splide( '#image-slider' ).mount(window.splide.Extensions);
-} );
+	var splide = new Splide( '#image-slider-pepper' ).mount(window.splide.Extensions);
+
+	splide.on( 'active', function( Splide ) {
+		stopVideos();
+	});
+
+	var splidemakex = new Splide( '#image-slider-makex' ).mount(window.splide.Extensions);
+
+	splidemakex.on( 'active', function( Splide ) {
+		stopVideos();
+	});
+});
+
+document.addEventListener("click", function(){
+	if(document.getElementById("modal-header")) {
+		console.log('ciao');
+		document.getElementById("modal-header").addEventListener("click", stopVideos);
+	}
+});
